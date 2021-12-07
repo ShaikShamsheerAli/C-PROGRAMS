77e31113b7afc466807c8c5d831c3391dc866781
ShaikShamsheerAli <95604424+ShaikShamsheerAli@users.noreply.github.com>
2021-12-07 16:07:35 +0530

    Update % of 5 subs.c

--- % of 5 subs.c
+++ % of 5 subs.c
@@ -7,23 +7,23 @@ int main()
 	scanf("%d%d%d%d%d",&phy,&mat,&vd,&bio,&comp);
 	per=(phy+mat+vd+bio+comp)/5;
 	printf("percentage=%.2f\n",per);
-	if(per>=90)
+        if(per>=90)
 	{
 		printf("grade A");
 	}
-	if(per>=80)
+	else if(per>=80)
 	{
 		printf("grade B");
 	}
-	if(per>=70)
+	else if(per>=70)
 	{
 		printf("grade C");
 	}
-	if(per>=40)
+	else if(per>=40)
 	{
 		printf("grade D");
 	}
-	if(per<=40)
+	else if(per<=40)
 	{
 		printf("grade E");
 	}
